3f30418ef07c6ca6b83f56d93c80b57d02669656
Andry <61607504+Andry1831006@users.noreply.github.com>
2020-10-11 23:20:28 +0700

    finish mockup v1

--- www/js/index.js
+++ www/js/index.js
@@ -1,8 +1,75 @@
-document.addEventListener("deviceready", onDeviceReady, false);
+const tabContentOrder = document.querySelector("#tab-content");
+const btnOngoing = document.querySelector("#ongoing");
+const btnPast = document.querySelector("#past");
 
-function onDeviceReady() {
-  // Cordova is now initialized. Have fun!
+btnOngoing.addEventListener("click", function () {
+  orderTabContent(btnOngoing, btnPast);
+  getContentOngoing();
+});
 
-  console.log("Running cordova-" + cordova.platformId + "@" + cordova.version);
-  document.getElementById("deviceready").classList.add("ready");
+btnPast.addEventListener("click", function () {
+  orderTabContent(btnPast, btnOngoing);
+  getContentPast();
+});
+
+function orderTabContent(a, b) {
+  a.classList.add("active");
+  b.classList.remove("active");
 }
+
+function getContentOngoing() {
+  tabContentOrder.innerHTML = `<div class="row tab-item mx-auto mt-3">
+    <div class="col wording">
+      <h5>XXX</h5>
+      <h6>Logo Design</h6>
+      <h4 class="mt-3">Rp. ###</h4>
+      <p class="text-primary">More Details</p>
+    </div>
+    <div class="col img-container">
+      <img src="img/d-logo.png" class="img-tab" />
+    </div>
+  </div>`;
+}
+function getContentPast() {
+  tabContentOrder.innerHTML = `<div class="row tab-item mx-auto mt-3">
+      <div class="col wording">
+        <h5>XXX</h5>
+        <h6>Logo Design</h6>
+        <h4 class="mt-3">Rp. ###</h4>
+        <p class="text-primary">More Details</p>
+      </div>
+      <div class="col img-container">
+        <img src="img/d-logo.png" class="img-tab" />
+      </div>
+    </div>
+    <div class="row tab-item mx-auto mt-3">
+      <div class="col wording">
+        <h5>XXX</h5>
+        <h6>Char Design</h6>
+        <h4 class="mt-3">Rp. ###</h4>
+        <p class="text-primary">More Details</p>
+      </div>
+      <div class="col img-container">
+        <img src="img/d-char.png" class="img-tab" />
+      </div>
+    </div>
+    <div class="row tab-item mx-auto mt-3">
+      <div class="col wording">
+        <h5>XXX</h5>
+        <h6>Game Design</h6>
+        <h4 class="mt-3">Rp. ###</h4>
+        <p class="text-primary">More Details</p>
+      </div>
+      <div class="col img-container">
+        <img src="img/d-game.png" class="img-tab" />
+      </div>
+    </div>`;
+}
+// document.addEventListener("deviceready", onDeviceReady, false);
+
+// function onDeviceReady() {
+// Cordova is now initialized. Have fun!
+
+//   console.log("Running cordova-" + cordova.platformId + "@" + cordova.version);
+//   document.getElementById("deviceready").classList.add("ready");
+// }
